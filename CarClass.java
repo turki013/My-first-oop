2a645ebf2017f85cfee591bcf8e17b7c2eafc4cb
turki013 <turki05318o@gmail.com>
2024-11-29 21:35:00 +0300

    Car

---
+++ CarClass.java
@@ -0,0 +1,21 @@
+public class CarClass {
+    private String maker;
+    private int model;
+
+    public void setmaker(String maker) {
+        this.maker = maker;
+    }
+
+    public void setmodel(int model) {
+        this.model = model;
+    }
+
+    public String getmaker() {
+        return maker;
+    }
+
+    public int getmodel() {
+        return model;
+    }
+    
+}
